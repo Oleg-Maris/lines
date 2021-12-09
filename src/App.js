77300410344b363d2b71b5673a588497d7ce5e8d
Oleg Maris <hipp55@ukr.net>
2021-12-09 23:18:13 +0200

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PlayingField from "./components/PlayingField";
+import "./App.css";
 
 function App() {
+  const cellsInitial = [];
+
+  for (let i = 0; i <= 80; i++) {
+    cellsInitial.push({ id: i, color: null, germ: false, isActive: false });
+  }
+
+  function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
+
+  const initialArray = () => {
+    let currentColor;
+    const idArray = [];
+    let n;
+    for (let i = 0; i < 7; i++) {
+      let m = randomInteger(1, 7);
+      do n = randomInteger(0, 80);
+      while (idArray.includes(n));
+      idArray.push(n);
+      // console.log("n", n);
+      // console.log("m", m);
+      console.log(idArray);
+      switch (m) {
+        case 1:
+          currentColor = "green";
+          break;
+        case 2:
+          currentColor = "yellow";
+          break;
+        case 3:
+          currentColor = "blue";
+          break;
+        case 4:
+          currentColor = "azure";
+          break;
+        case 5:
+          currentColor = "purple";
+          break;
+        case 6:
+          currentColor = "red";
+          break;
+        default:
+          currentColor = "brown";
+          break;
+      }
+      // debugger;
+      cellsInitial[n].color = currentColor;
+    }
+    for (let i = 0; i < 3; i++) {
+      let m = randomInteger(1, 7);
+      do n = randomInteger(0, 80);
+      while (idArray.includes(n));
+      idArray.push(n);
+      // console.log("n", n);
+      // console.log("m", m);
+      console.log(idArray);
+      switch (m) {
+        case 1:
+          currentColor = "green";
+          break;
+        case 2:
+          currentColor = "yellow";
+          break;
+        case 3:
+          currentColor = "blue";
+          break;
+        case 4:
+          currentColor = "azure";
+          break;
+        case 5:
+          currentColor = "purple";
+          break;
+        case 6:
+          currentColor = "red";
+          break;
+        default:
+          currentColor = "brown";
+          break;
+      }
+      cellsInitial[n].color = currentColor;
+      cellsInitial[n].germ = true;
+    }
+  };
+
+  initialArray();
+
+  // useEffect(() => {
+  //   initialArray();
+  // }, []);
+
+  const show = () => {
+    console.log(cellsInitial);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Lines 98</header>
+      <PlayingField cells={cellsInitial} />
+      <button onClick={show}>Show</button>
     </div>
   );
 }
---
+++ src/components/PlayingField.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Cell from "./Cell";
+import Ball from "./Ball";
+import styles from "./PlayingField.module.css";
+
+const PlayingField = (props) => {
+  return (
+    <div className={styles.field}>
+      {props.cells.map((item) => (
+        <Cell key={item.id}>
+          {item.color && (
+            <Ball
+              color={item.color}
+              germ={item.germ}
+              isActive={item.isActive}
+            />
+          )}
+        </Cell>
+      ))}
+    </div>
+  );
+};
+
+export default PlayingField;
